2830191ae63c81db9ff1691828091661ecf71e54
JanOstrowski1 <jan.ostrowski1337@gmail.com>
2023-02-15 21:01:58 +0100

    added error handling

--- zadanie/lib/main.dart
+++ zadanie/lib/main.dart
@@ -4,82 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final textController = TextEditingController();
-  String result = '';
-
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
-  }
-
-  void onButtonPressed() {
-    try {
-      List<int> numbers =
-          textController.text.split(',').map(int.parse).toList();
-      int outlier = findOutlier(numbers);
-      if (outlier != -1) {
-        setState(() {
-          result = 'Liczba odstająca to $outlier';
-        });
-      } else {
-        setState(() {
-          result = 'Nie znaleziono liczby odstającej';
-        });
-      }
-    } on FormatException catch (_) {
-      setState(() {
-        result = 'Podane liczby zostały wpisane niepoprawnie';
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Wyszukiwanie liczby odstającej',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wyszukiwanie liczby odstającej '),
-        ),
-        body: Container(
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              TextField(
-                decoration: const InputDecoration(
-                    hintText: 'Wpisz liczby oddzielone przecinkiem'),
-                controller: textController,
-              ),
-              ElevatedButton(
-                onPressed: onButtonPressed,
-                child: const Text('Wyszukaj'),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                result,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 int findOutlier(List<int> numbers) {
   int evenCount = 0;
   int oddCount = 0;
@@ -100,5 +24,90 @@ int findOutlier(List<int> numbers) {
       return even;
     }
   }
-  return -1;
+  return -1; // błąd, nie znaleziono liczby odstającej
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final textController = TextEditingController();
+  String result = '';
+  String errorMessage = '';
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  void onButtonPressed() {
+    try {
+      List<int> numbers = textController.text.split(',').map(int.parse).toList();
+      int outlier = findOutlier(numbers);
+      if (outlier != -1) {
+        setState(() {
+          result = 'Liczba odstająca to $outlier';
+          errorMessage = '';
+        });
+      } else {
+        setState(() {
+          result = 'Nie znaleziono liczby odstającej';
+          errorMessage = '';
+        });
+      }
+    } on FormatException catch (_) {
+      setState(() {
+        errorMessage = 'Podane liczby zostały wpisane niepoprawnie';
+        result = '';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wyszukiwanie liczby odstającej',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Wyszukiwanie liczby odstającej'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Wpisz liczby oddzielone przecinkiem'),
+                controller: textController,
+              ),
+              ElevatedButton(
+                onPressed: onButtonPressed,
+                child: Text('Wyszukaj'),
+              ),
+              SizedBox(height: 20),
+              if (errorMessage.isNotEmpty)
+                Text(
+                  errorMessage,
+                  style: TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              if (result.isNotEmpty)
+                Text(
+                  result,
+                  style: TextStyle(fontSize: 20),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
